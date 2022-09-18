67afa63a76ad6b49b600c5735d39c2a7ed99f466
EfimovE <efimov_e@mail.ru>
2022-09-18 21:33:27 +0500

    create main program

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[4] {":)", "Russia", "123", "world"};
+string[] array2 = new string[array.Length];
+
+void CreateResultArray(string[] array, string[] array2)
+{
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            array2[index] = array[i];
+            index ++;
+        }
+    }
+}
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+CreateResultArray(array, array2);
+PrintArray(array);
+PrintArray(array2);
